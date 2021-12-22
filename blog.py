dc8f7b9d966f804c625c6620b77ee7b323b2a7d5
vagrant <vagrant@vagrant.vm>
2021-12-22 20:10:46 +0000

    Added SQLAlchemy and models module.

--- blog.py
+++ blog.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import psql_login
+from models import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = psql_login
+db.init_app(app)
+
 
 @app.route("/")
 def hello_world():
