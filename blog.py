a246e409ceca2fc3c17b97f0f6718d257ec59f21
vagrant <vagrant@vagrant.vm>
2021-12-23 20:12:11 +0000

    Added function for signing up.

--- blog.py
+++ blog.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from config import psql_login
 from models import db
@@ -13,6 +13,13 @@ db.init_app(app)
 @app.route("/")
 def hello_world():
     return "<p>Hello World!</p>"
+
+
+@app.route("/test", methods=["GET","POST"])
+def test():
+    if request.method == "GET":
+        return render_template("signup.html")
+
 
 # app.debug = True
 # app.run(host='0.0.0.0', port=5000)
