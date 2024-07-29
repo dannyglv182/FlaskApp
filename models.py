a2ddc688037dcce5abdc23659d8e7aea1833aada
Daniel Galvez <dannyglv182@gmail.com>
2024-07-28 17:22:31 -0700

    Update models.py
    fix context problem in models file

--- models.py
+++ models.py
@@ -1,10 +1,15 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+from config import psql_login
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = psql_login
+db = SQLAlchemy(app)
 
 class app_user(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(32), nullable=False)
-    password = db.Column(db.Integer, 
+    password = db.Column(db.Integer,
                          db.ForeignKey("p_word.id"),
                          unique=True,
                          nullable=False)
@@ -21,3 +26,5 @@ class p_word(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     hash_value = db.Column(db.String(140), nullable=False)
 
+
+# db.create_all()
