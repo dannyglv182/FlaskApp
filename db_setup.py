9292b52235b9adcde264ffa1010470ae8563fb08
Daniel Galvez <dannyglv182@gmail.com>
2024-07-28 17:20:55 -0700

    Update db_setup.py
    fix context problem when creating tables

--- db_setup.py
+++ db_setup.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import psql_login
+# from config import psql_login
 
+db = SQLAlchemy()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = psql_login
-db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://vagrant:12345@localhost:5432/vagrant"
+db.init_app(app)
 
 class app_user(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(32), nullable=False)
-    password = db.Column(db.Integer, 
+    password = db.Column(db.Integer,
                          db.ForeignKey("p_word.id"),
                          unique=True,
                          nullable=False)
@@ -26,5 +27,5 @@ class p_word(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     hash_value = db.Column(db.String(140), nullable=False)
 
-
-db.create_all()
+with app.app_context():
+    db.create_all()
